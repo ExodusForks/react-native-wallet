7459691175b626f4e0a278ca69cc04e77629c4ab
Joshua Rogers <joshua.ro@exodus.io>
2026-02-01 20:03:13 +0800

    Fix typo

--- src/index.tsx
+++ src/index.tsx
@@ -156,7 +156,7 @@ async function addCardToAppleWallet(
 
   async function addPaymentPassToWallet(paymentPassData: IOSAddPaymentPassData): Promise<number> {
     const responseData = await issuerEncryptPayloadCallback(paymentPassData.nonce, paymentPassData.nonceSignature, paymentPassData.certificates);
-    const response = await Wallet.IOSHandleAddPaymentPassResponse(responseData);
+    const response = await wallet.IOSHandleAddPaymentPassResponse(responseData);
     // Response is null when a pass is successfully added to the wallet or the user cancels the process
     // In case the user presses the `Try again` option, new pass data is returned, and it should reenter the function
     if (response) {
