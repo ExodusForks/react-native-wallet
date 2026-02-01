eb76c45ea87791de9169dbf156cc53298c1ffd39
Joshua Rogers <joshua.ro@exodus.io>
2026-02-01 19:48:09 +0800

    Fix typo

--- src/index.tsx
+++ src/index.tsx
@@ -140,7 +140,7 @@ async function addCardToAppleWallet(
   issuerEncryptPayloadCallback: (nonce: string, nonceSignature: string, certificate: string[]) => Promise<IOSEncryptPayload>,
 ): Promise<TokenizationStatus> {
   if (Platform.OS === 'android') {
-    throw new Error('addCardToAppleWallet is not available on Andorid');
+    throw new Error('addCardToAppleWallet is not available on Android');
   }
 
   if (!Wallet) {
